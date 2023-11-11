7ff1fcdfe40af5b4d9fe1a147d4320b1e475a450
Shivani <shivani.2125it1065@kiet.edu>
2023-11-11 18:57:18 +0530

    Time: 1 ms (86.52%), Space: 40.7 MB (25.31%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -12,33 +12,14 @@
 //     }
 // }
 
-// class Solution {
-//     public boolean checkIfPangram(String sentence) {
-//         int[] arr = new int[26];
-        
-//         // checks the frequency of the alphabets
-//         for(var i: sentence.toCharArray()){
-//             arr[i - 'a']++;
-//         }
-        
-//         // checks if any of the arr index contains zero
-//         for(var i: arr){
-//             if(i == 0)
-//                 return false;
-//         }
-        
-//         return true;
-//     }
-// }
-
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        int[] check = new int[26];
-        for(var i : sentence.toCharArray()){
-            check[i-'a']++;
+        boolean[] check = new boolean[26];
+        for(int i : sentence.toCharArray()){
+            check[i-'a'] = true;
         }
-        for(var i : check){
-            if(i==0)
+        for(boolean i : check){
+            if(!i)
                 return false;
         }
         return true;
