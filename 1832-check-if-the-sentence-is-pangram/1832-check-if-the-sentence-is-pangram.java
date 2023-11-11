35bb63530e46ac66f157af683e8b956209f96f5a
Shivani <shivani.2125it1065@kiet.edu>
2023-11-11 18:58:30 +0530

    Time: 2 ms (73.94%), Space: 41 MB (11.58%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,22 +1,8 @@
-// class Solution {
-//     public boolean checkIfPangram(String sentence) {
-//         boolean[] check = new boolean[26];
-//         for(int i=0 ; i<sentence.length() ; i++){
-//             check[sentence.charAt(i) - 'a'] = true;
-//         }
-//         for(boolean i : check){
-//             if(!i)
-//                 return false;
-//         }
-//         return true;
-//     }
-// }
-
 class Solution {
     public boolean checkIfPangram(String sentence) {
         boolean[] check = new boolean[26];
-        for(int i : sentence.toCharArray()){
-            check[i-'a'] = true;
+        for(int i=0 ; i<sentence.length() ; i++){
+            check[sentence.charAt(i) - 'a'] = true;
         }
         for(boolean i : check){
             if(!i)
@@ -25,3 +11,17 @@ class Solution {
         return true;
     }
 }
+
+// class Solution {
+//     public boolean checkIfPangram(String sentence) {
+//         boolean[] check = new boolean[26];
+//         for(int i=0 ; i<sentence.length() ; i++){
+//             check[i-'a'] = true;
+//         }
+//         for(boolean i : check){
+//             if(!i)
+//                 return false;
+//         }
+//         return true;
+//     }
+// }
