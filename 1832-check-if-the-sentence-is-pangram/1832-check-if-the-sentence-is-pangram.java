f578101a3dd00a372e8cd15b1629a6731d7415c4
Shivani <shivani.2125it1065@kiet.edu>
2023-11-11 17:46:31 +0530

    Time: 2 ms (73.94%), Space: 40.2 MB (85.94%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        boolean[] check = new boolean[26];
+        for(int i=0 ; i<sentence.length() ; i++){
+            check[sentence.charAt(i) - 'a'] = true;
+        }
+        for(boolean i : check){
+            if(!i)
+                return false;
+        }
+        return true;
+    }
+}
