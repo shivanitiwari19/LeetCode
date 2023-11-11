39cc4fbc1c4d2426bd3409eb1f1e615988f0f6cb
Shivani <shivani.2125it1065@kiet.edu>
2023-11-11 18:54:14 +0530

    Time: 1 ms (86.52%), Space: 40.8 MB (25.31%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,11 +1,44 @@
+// class Solution {
+//     public boolean checkIfPangram(String sentence) {
+//         boolean[] check = new boolean[26];
+//         for(int i=0 ; i<sentence.length() ; i++){
+//             check[sentence.charAt(i) - 'a'] = true;
+//         }
+//         for(boolean i : check){
+//             if(!i)
+//                 return false;
+//         }
+//         return true;
+//     }
+// }
+
+// class Solution {
+//     public boolean checkIfPangram(String sentence) {
+//         int[] arr = new int[26];
+        
+//         // checks the frequency of the alphabets
+//         for(var i: sentence.toCharArray()){
+//             arr[i - 'a']++;
+//         }
+        
+//         // checks if any of the arr index contains zero
+//         for(var i: arr){
+//             if(i == 0)
+//                 return false;
+//         }
+        
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        boolean[] check = new boolean[26];
-        for(int i=0 ; i<sentence.length() ; i++){
-            check[sentence.charAt(i) - 'a'] = true;
+        int[] check = new int[26];
+        for(var i : sentence.toCharArray()){
+            check[i-'a']++;
         }
-        for(boolean i : check){
-            if(!i)
+        for(var i : check){
+            if(i==0)
                 return false;
         }
         return true;
