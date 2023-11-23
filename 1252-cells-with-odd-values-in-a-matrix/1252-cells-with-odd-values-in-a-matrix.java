d01d509639bc520d12da0927c772caf0a67bba0a
Shivani <shivani.2125it1065@kiet.edu>
2023-11-23 23:37:21 +0530

    Time: 2 ms (64.92%), Space: 40.8 MB (19.75%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -13,8 +13,8 @@ class Solution {
                 res[j][b]++;
         }
         int count=0;
-        for(int i=0 ; i<m ; i++){
-            for(int j=0 ; j<n ; j++){
+        for(int i=0 ; i<res.length ; i++){
+            for(int j=0 ; j<res[i].length ; j++){
                 if(res[i][j] % 2 == 1)
                     count++;
             }
