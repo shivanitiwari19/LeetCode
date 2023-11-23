0b188a89d2b755b3f239c7b764f4bca0c4095800
Shivani <shivani.2125it1065@kiet.edu>
2023-11-23 20:54:54 +0530

    Time: 18 ms (56.87%), Space: 44.9 MB (9.62%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -1,26 +1,25 @@
 class Solution {
-    public Boolean check(int[] arr) {
-        Arrays.sort(arr);
-        int diff = arr[1] - arr[0];
-        
-        for (int i = 2; i < arr.length; i++) {
-            if (arr[i] - arr[i - 1] != diff) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-    
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        List<Boolean> ans = new ArrayList();
-        for (int i = 0; i < l.length; i++) {
-            int[] arr = new int[r[i] - l[i] + 1];
-            for (int j = 0; j < arr.length; j++) {
-                arr[j] = nums[l[i] + j];
+        List<Boolean> ans = new ArrayList<>();
+        int m = l.length;
+
+        for (int i = 0; i < m; i++) {
+            int[] v = Arrays.copyOfRange(nums, l[i], r[i] + 1);
+            Arrays.sort(v);
+
+            boolean isArith = true;
+            if (v.length > 1) {
+                int diff = v[1] - v[0];
+                for (int j = 1; j < v.length; j++) {
+                    if (v[j] - v[j - 1] != diff) {
+                        isArith = false;
+                        break;
+                    }
+                }
+            } else {
+                isArith = false;
             }
-            
-            ans.add(check(arr));
+            ans.add(isArith);
         }
 
         return ans;
