5e8d6aaa3dbd566535a014ab00f88029df08f43d
Shivani <shivani.2125it1065@kiet.edu>
2023-11-27 12:28:29 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (36.86%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
@@ -7,37 +7,37 @@
  * int guess(int num);
  */
 
-// public class Solution extends GuessGame {
-//     public int guessNumber(int n) {
-//         int low = 1;
-//         int high = n;
-//         int mid = low + (high-low)/2;
-//         while(low<high){
-//             if (guess(mid)==-1)
-//                 high = mid-1;
-//             else if (guess(mid)==1)
-//                 low=mid+1;
-//             else 
-//                 return mid;
-//         }
-//         return -1;
-//     }
-// }
-
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-        int first = 1, last = n;
-        while (first <= last) {
-            int mid = first + (last - first) / 2;
-            int res = guess(mid);
-            if (res == 0) {
+        int low = 1;
+        int high = n;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if (guess(mid)==0)
                 return mid;
-            } else if (res == -1) {
-                last = mid - 1;
-            } else {
-                first = mid + 1;
-            }
+            else if (guess(mid)==-1)
+                high=mid-1;
+            else 
+                low = mid+1;
         }
         return -1;
     }
 }
+
+// public class Solution extends GuessGame {
+//     public int guessNumber(int n) {
+//         int first = 1, last = n;
+//         while (first <= last) {
+//             int mid = first + (last - first) / 2;
+//             int res = guess(mid);
+//             if (res == 0) {
+//                 return mid;
+//             } else if (res == -1) {
+//                 last = mid - 1;
+//             } else {
+//                 first = mid + 1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
