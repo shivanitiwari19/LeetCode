54270006f63e95101452763bd80c3c907c2f050d
Shivani <shivani.2125it1065@kiet.edu>
2023-11-29 23:27:06 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (55.34%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,10 +1,10 @@
 public class Solution {
     public int hammingWeight(int n) {       
-		int set = 0;        
+		int count = 0;        
 		while (n != 0) {
             n &= (n - 1); // to clear the right most set bit
-            ++set;
+            ++count;
         }		
-        return set;
+        return count;
     }
 }
