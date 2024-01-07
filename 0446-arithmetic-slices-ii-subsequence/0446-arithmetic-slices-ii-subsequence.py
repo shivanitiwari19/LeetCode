009e385e5154cfe339c52508d97e95532f968956
Shivani <shivani.2125it1065@kiet.edu>
2024-01-07 23:33:32 +0530

    Time: 465 ms (78.57%), Space: 61 MB (28.57%) - LeetHub

---
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def numberOfArithmeticSlices(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        total_count = 0  
+        dp = [defaultdict(int) for _ in range(n)]
+
+        for i in range(1, n):
+            for j in range(i):
+                diff = nums[i] - nums[j]
+                dp[i][diff] += 1  
+                if diff in dp[j]:
+                    dp[i][diff] += dp[j][diff]
+                    total_count += dp[j][diff]
+
+        return total_count
+        
+        
