29ab936c1ffd0a35dd4b6f18b4a5b0e538401588
Shivani <shivani.2125it1065@kiet.edu>
2024-02-20 23:56:55 +0530

    Time: 3 ms (97.86%), Space: 60.4 MB (5.64%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -5,9 +5,9 @@ class Solution {
         res[v[0] - 1] += v[2];
         if (v[1] < n)  
             res[v[1]] -= v[2];
-    }
-    for (int i = 1; i < n; ++i) 
-        res[i] += res[i - 1];
-    return res;
+        }
+        for (int i = 1; i < n; ++i) 
+            res[i] += res[i - 1];
+        return res;
     }
 }
