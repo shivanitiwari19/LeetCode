facd03637e82db761324233a55f8dd77f933bb33
Shivani <shivani.2125it1065@kiet.edu>
2024-03-01 23:36:59 +0530

    Time: 2 ms (83.47%), Space: 43.2 MB (77.78%) - LeetHub

---
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String maximumOddBinaryNumber(String s) {
+        int count = -1; 
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0 ; i<s.length() ; i++){
+            count += (s.charAt(i) == '1') ? 1 : 0;
+        }
+        for(int i = 0 ; i<s.length() ; i++, count--){
+            ans.append(count > 0 ? '1' : '0');
+        }
+        ans.deleteCharAt(ans.length() - 1);
+        ans.append('1');
+        return ans.toString();
+    }
+}
