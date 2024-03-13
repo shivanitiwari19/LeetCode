609ef37766a916d7b0c170d15da3af54a9a15867
Shivani <shivani.2125it1065@kiet.edu>
2024-03-13 23:11:26 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (43.66%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,9 +1,8 @@
 class Solution {
     public int pivotInteger(int n) {
-        double a = Math.sqrt(n*(n+1)/2);
         
-        if(a - Math.ceil(a) == 0)
-            return (int) a;
+        if(Math.sqrt(n*(n+1)/2) - Math.ceil(Math.sqrt(n*(n+1)/2)) == 0)
+            return (int) Math.sqrt(n*(n+1)/2);
         else 
             return -1;
     }
