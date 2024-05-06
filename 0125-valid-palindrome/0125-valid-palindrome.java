bb7ba82dc14cc3e5d06ae08a75b63f90b954d2a3
Shivani <shivani.2125it1065@kiet.edu>
2024-05-07 00:58:37 +0530

    Time: 3 ms (74.18%), Space: 42.5 MB (91.60%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,39 +1,49 @@
-// class Solution {
-//     public boolean isPalindrome(String s) {
-//         s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
-//         int left = 0;
-//         int right = s.length() - 1;
-//         while(left<right){
-//             if(s.charAt(left) != s.charAt(right))
-//                 return false;
-//             left++;
-//             right--;
-//         }
-//         return true;
-//     }
-// }
-
 class Solution {
     public boolean isPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
-        
-        while (left < right) {
-            char leftChar = Character.toLowerCase(s.charAt(left));
-            char rightChar = Character.toLowerCase(s.charAt(right));
-            
-            if (!Character.isLetterOrDigit(leftChar)) {
+        while(left<right){
+            char lc = Character.toLowerCase(s.charAt(left));
+            char rc = Character.toLowerCase(s.charAt(right));
+            if(!Character.isLetterOrDigit(lc)){
                 left++;
-            } else if (!Character.isLetterOrDigit(rightChar)) {
+            }
+            else if(!Character.isLetterOrDigit(rc)){
                 right--;
-            } else {
-                if (leftChar != rightChar)
+            }
+            else{
+                if(lc != rc){
                     return false;
+                }
                 left++;
                 right--;
             }
         }
-        
         return true;
     }
 }
+
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         int left = 0;
+//         int right = s.length() - 1;
+        
+//         while (left < right) {
+//             char leftChar = Character.toLowerCase(s.charAt(left));
+//             char rightChar = Character.toLowerCase(s.charAt(right));
+            
+//             if (!Character.isLetterOrDigit(leftChar)) {
+//                 left++;
+//             } else if (!Character.isLetterOrDigit(rightChar)) {
+//                 right--;
+//             } else {
+//                 if (leftChar != rightChar)
+//                     return false;
+//                 left++;
+//                 right--;
+//             }
+//         }
+        
+//         return true;
+//     }
+// }
