77b6dd3881682b677a9451c7cb918ee5567db852
Shivani <shivani.2125it1065@kiet.edu>
2024-05-07 00:54:00 +0530

    Time: 3 ms (74.18%), Space: 42.5 MB (95.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,39 @@
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+//         int left = 0;
+//         int right = s.length() - 1;
+//         while(left<right){
+//             if(s.charAt(left) != s.charAt(right))
+//                 return false;
+//             left++;
+//             right--;
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
         int left = 0;
         int right = s.length() - 1;
-        while(left<right){
-            if(s.charAt(left) != s.charAt(right))
-                return false;
-            left++;
-            right--;
+        
+        while (left < right) {
+            char leftChar = Character.toLowerCase(s.charAt(left));
+            char rightChar = Character.toLowerCase(s.charAt(right));
+            
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+            } else {
+                if (leftChar != rightChar)
+                    return false;
+                left++;
+                right--;
+            }
         }
+        
         return true;
     }
 }
